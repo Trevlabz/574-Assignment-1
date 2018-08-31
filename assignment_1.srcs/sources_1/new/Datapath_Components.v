0129d5058fad46cd53361a35a2d7a4c38573a810
Trevlabz <trevlabz@email.arizona.edu>
2018-08-30 20:47:52 -0700

    Initial commit
    Created project folder
    Added assignment detail pdfs

---
+++ assignment_1.srcs/sources_1/new/Datapath_Components.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ns
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Trevor LaBanz
+// 
+// Create Date: 08/30/2018 08:24:51 PM
+// Design Name: Assignment 1
+// Module Name: Datapath_Components
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Datapath_Components(
+
+    );
+endmodule
