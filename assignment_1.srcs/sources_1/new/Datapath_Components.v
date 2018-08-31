7888809596ec41d91d7db2dea87e77a1504a8e3c
Trevlabz <trevlabz@email.arizona.edu>
2018-08-30 22:40:39 -0700

    Created REG and ADD
    created and verified REG and ADD modules

--- assignment_1.srcs/sources_1/new/Datapath_Components.v
+++ assignment_1.srcs/sources_1/new/Datapath_Components.v
@@ -19,7 +19,38 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Datapath_Components(
-
-    );
+//Register
+//DataIn(d)
+//CtrlIn(Clk,Rst)
+//DataOut(q)
+//CtrlOut()
+//Parameter(DATAWIDTH)
+module REG #(parameter DATAWIDTH = 2)(d, Clk, Rst, q);
+    input [DATAWIDTH-1:0] d;
+    input Clk, Rst;
+    output reg [DATAWIDTH-1:0] q;
+    
+    always @(posedge Clk) begin
+        if(Rst == 1) //sync reset
+            q <= {DATAWIDTH{1'b0}};
+        else
+            q <= d; 
+    end
 endmodule
+
+
+//Adder
+//DataIn(a,b)
+//CtrlIn()
+//DataOut(sum)
+//CtrlOut()
+//Parameter(DATAWIDTH)
+module ADD #(parameter DATAWIDTH = 2)(a, b, sum);
+    input [DATAWIDTH-1:0] a, b;
+    output reg [DATAWIDTH-1:0] sum;
+    
+    always @(a, b) begin
+        sum <= a + b; //add a and b        
+    end
+endmodule
+
